f6a73616c8a9d98054eeca0b7171a4516c5611f1
Klouz <yuliavydryk@gmail.com>
2024-05-05 15:51:35 +0300

    Setting up the content of the site

--- js/units/droplist-shortInfo.js
+++ js/units/droplist-shortInfo.js
@@ -2,20 +2,22 @@ const shortInfoDropListTitle = document.querySelector('.short-info--droplist-tit
 const shortInfoDropListTitleBounds = document.querySelector('.short-info--droplist');
 const shortInfoDropList = document.querySelector('.short-info--droplist-list');
 
-const changeDroplist = function(opacity, roundAngles){
-    shortInfoDropList.style.opacity = opacity;
+// shortInfoDropList.style.display = 'none';
+
+const changeDroplist = function(displayMode, roundAngles){
+    shortInfoDropList.style.display = displayMode;
     shortInfoDropListTitleBounds.style.borderBottomLeftRadius = roundAngles;
     shortInfoDropListTitleBounds.style.borderBottomRightRadius = roundAngles;
 }
 
 const droplistCheck = function(){
     // console.log(shortInfoDropListTitle);
-    if(window.getComputedStyle(shortInfoDropList).getPropertyValue('opacity') === '1') changeDroplist('0', '1vw');
-    else changeDroplist('1', '0');
+    if(window.getComputedStyle(shortInfoDropList).getPropertyValue('display') === 'flex') changeDroplist('none', '1vw');
+    else changeDroplist('flex', '0');
 }
 
 shortInfoDropListTitle.addEventListener('click', droplistCheck);
 document.body.addEventListener('click', function(e){
     // console.log(e.target);
-    if(e.target !== shortInfoDropListTitle) changeDroplist('0', '1vw');
+    if(e.target !== shortInfoDropListTitle) changeDroplist('none', '1vw');
 })
